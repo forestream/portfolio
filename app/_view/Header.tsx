c3d67c78eeb8c1408f2529532b34150b85873f22
hanbin <rj2303@naver.com>
2025-04-14 11:31:04 +0900

    SplitFlap 컴포넌트 수정

--- app/_view/Header.tsx
+++ app/_view/Header.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import SplitFlap from "@/components/SplitFlap";
+import { useSplitFlapContext } from "@/hooks/useSplitFlapContext";
+import { useMemo } from "react";
 
 export default function Header() {
+  const { text } = useSplitFlapContext();
+  const splitText = useMemo(() => text.replaceAll(" ", ""), [text]);
+
   return (
     <header className="fixed top-0 right-0 left-0 z-10 px-2 py-2 md:px-12 md:py-4">
-      <SplitFlap fontSize={24} />
+      <SplitFlap word={splitText} fontSize={24} width={40} height={48} />
       <hr className="mt-4 border border-orange-500/50" />
     </header>
   );
